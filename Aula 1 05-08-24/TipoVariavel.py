4f252a274d3057cc080aab9a46bb7df3de7f19bf
Welly Rudnick <wellysonrudnick13@gmail.com>
2024-08-05 17:25:00 -0300

    .

---
+++ Aula 1 05-08-24/TipoVariavel.py
@@ -0,0 +1,7 @@
+codigo = 10
+salario = 1500.00
+nome= "José"
+
+print("\nCodigo:", codigo, "\nnome:", nome, "\nO salário atual é de ", salario)
+
+print("\nCódigo: " + str(codigo) + "\nNome: " + nome + "\nO salário atual é de " + str(salario))
