909f36fde7c72b72589bc70f001ad7e235297fa2
Welly <wellysonrudnick13@gmail.com>
2024-09-27 17:27:26 -0300

    27-09-24

---
+++ Aula 8 27-09-24/Exercicio/Exercicio1.py
@@ -0,0 +1,34 @@
+contador = 0
+minha_lista = []
+
+def incrementar_contador():
+    global contador
+    contador += 1
+    return contador
+
+def exibir_contador():
+    global contador
+    print(contador)
+
+def adicionar_na_lista(item, lista):
+    global minha_lista
+    n = int(input("Digite o número de itens a serem adicionados: "))
+    for i in range(n):
+        item = input("Digite o item a ser adicionado: ")
+        lista.append(item)
+    return minha_lista
+
+def exibir_lista(minha_lista):
+    print(f"Itens da lista: {minha_lista}")
+
+def main():
+    global minha_lista
+    adicionar_na_lista(None, minha_lista)
+    exibir_lista(minha_lista)
+    incrementar_contador()
+    print(f"Contador atualizado: {contador}")
+    incrementar_contador()
+    print(f"Contador atualizado: {contador}")
+    print(f"Valor atual do contador: {contador}")
+
+main()
